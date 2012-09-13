99e1f9e3bbbc91c33a201e2d2a21e3efbbb15177
mtop <mats@topel.se>
2012-09-13 15:09:01 +0200

    Fixed a bug in the line count

--- z2mb.py
+++ z2mb.py
@@ -3,16 +3,21 @@
 import sys
 
 in_file = sys.argv[1]
-cutoff = sys.argv[2]
+cutoff = float(sys.argv[2])
+print type(cutoff)			# Devel
+print cutoff				# Devel.
 file = open(in_file, "r")
 line_nr = 1
 excluded = "F"
+
+# For the MrBayes block
 print "exclude",
 for line in file:
+	line = float(line)
 	if line < cutoff:
 		print line_nr,
-		line_nr += 1
 		excluded = "T"
+	line_nr += 1
 if excluded == "F":
 	print "all",
 print ";"
