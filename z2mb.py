77344e8717ddc36c7fcd569c1c2489c2433689aa
mtop <mats@topel.se>
2012-09-13 08:20:33 +0100

    Added z2mb.py

---
+++ z2mb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+
+in_file = sys.argv[1]
+cutoff = sys.argv[2]
+file = open(in_file, "r")
+line_nr = 1
+exluded_sites = []
+
+for line in file:
+	if line < cutoff
+	exluded_sites.append(line)
+print excluded_sites	
