ded5f7fa1fdb2f9d15e80e0bde7cf43dc1a46e61
mtop <mats@topel.se>
2012-09-13 09:57:29 +0200

    z2mb.py now functional

--- z2mb.py
+++ z2mb.py
@@ -6,9 +6,12 @@ in_file = sys.argv[1]
 cutoff = sys.argv[2]
 file = open(in_file, "r")
 line_nr = 1
-exluded_sites = []
-
+excluded = "F"
+print "exclude",
 for line in file:
-	if line < cutoff
-	exluded_sites.append(line)
-print excluded_sites	
+	if line < cutoff:
+		print line_nr,
+		line_nr += 1
+		excluded = "T"
+if excluded == "F":
+	print "all"
