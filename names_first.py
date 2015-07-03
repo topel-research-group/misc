a98336cc819c71c525ce9b35f995772fc10e65d3
Mats Töpel <mats@topel.se>
2015-07-03 09:21:46 +0200

    First commit after fork of LEGACY code

--- names_first.py
+++ names_first.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 
-# Syntax:	names_first.py all.mafft.fst > all.mafft.names.fst
-
+# This program takes a fasta file as input and tries to 
+# move the sequence name to the beginning of the line. 
+# This simplifies viewing of multiple sequence alignments 
+# in alignments editors like SeaView.
+#
+# Usage:	names_first.py all.mafft.fst > all.mafft.names.fst
 
 import sys
-
-in_file = sys.argv[1]  # Input xml file
-
+in_file = sys.argv[1]  
 file = open(in_file, "r")
 
 for line in file:
 	if line[0] == ">":
-		if "@" in line:
-			line.split("@")
-			first = line.split("@")[0]
-			print "%s%s_%s%s" % (">", line.split("@")[1], first.split(">")[1], line.split("@")[2])
+		if "[" in line:
+			first = line.split("[")[0].replace(">","")
+			name = line.split("[")[1].split("]")[0]
+			last = line.split("]")[1]
+			name = name.replace(" ", "_")
+			print "%s%s_%s%s" % (">", name, first, last)
 		else:
 			print line,
 	else:
